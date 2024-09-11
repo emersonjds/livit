0b9a8890f261ad96f1f3176eeabd0726bf0ad632
Emerson Silva <emerson_jdss@hotmail.com>
2024-09-11 19:50:44 -0300

    validate gluestack configuration

--- App.tsx
+++ App.tsx
@@ -2,12 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import "@/global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { StyleSheet, Text, View } from "react-native";
+import Example from "./Example";
 
 export default function App() {
   return (
     <GluestackUIProvider mode="light">
       <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
+        {/* <Text>Open up App.tsx to start working on your app!</Text> */}
+        <Example />
         <StatusBar style="auto" />
       </View>
     </GluestackUIProvider>
