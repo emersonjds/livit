21cf38702dca4c863e33d0c0a8c719e1dc8532f1
Emerson Silva <emerson_jdss@hotmail.com>
2024-10-01 00:33:59 -0300

    adjust imports

--- App.tsx
+++ App.tsx
@@ -1,23 +1,15 @@
-import { StatusBar } from "expo-status-bar";
+// import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import "@styles/global.css";
+import { StatusBar } from "expo-status-bar";
 
-import "@/styles/global.css";
+import Home from "./src/app/Home";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <>
+      <Home />
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ src/app/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function Home() {
+  return (
+    <View className="flex-1 justify-center items-center bg-gray-500">
+      <Text className="font-white text-center">
+        Open up App.tsx to start working on your app!
+      </Text>
+    </View>
+  );
+}
