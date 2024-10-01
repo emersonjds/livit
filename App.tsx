a8af1b9e53496d86d129dc9d32861397e859dc51
Emerson Silva <emerson_jdss@hotmail.com>
2024-10-01 00:04:06 -0300

    adjust new project

--- App.tsx
+++ App.tsx
@@ -1,26 +1,20 @@
-import { StatusBar } from "expo-status-bar";
-import "@/global.css";
-import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
-import { StyleSheet, Text, View } from "react-native";
-import Example from "./Example";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <GluestackUIProvider mode="light">
-      <View style={styles.container}>
-        {/* <Text>Open up App.tsx to start working on your app!</Text> */}
-        <Example />
-        <StatusBar style="auto" />
-      </View>
-    </GluestackUIProvider>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
